fe4810f0f02339b8af3781d09f593072163acf68
zqpie <75803315+zqpie@users.noreply.github.com>
2021-10-09 17:48:40 -0400

    Add files via upload

--- Godot Basic Move And Dodge/scripts/Steve-ball.gd
+++ Godot Basic Move And Dodge/scripts/Steve-ball.gd
@@ -16,8 +16,6 @@ func _physics_process(delta):
 		velocity.z = 0
 		velocity.x = -8
 		
-	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
-		velocity.y = 0
 	elif Input.is_action_pressed("ui_up"):
 		$MeshInstance.rotate_x(deg2rad(-ROTSPEED))
 		velocity.x = 0
@@ -26,6 +24,8 @@ func _physics_process(delta):
 		$MeshInstance.rotate_x(deg2rad(ROTSPEED))
 		velocity.x = 0
 		velocity.z = 8
+	#else:
+	#	velocity.x = 0
 	#elif Input.is_action_pressed("ui_up"):
 	#	height.y = 5 # Sets the position of X to 10
 	#else:
@@ -36,14 +36,19 @@ func _physics_process(delta):
 	
 	
 	if velocity.x > 0:  # calculate friction
-		velocity.x -= .05
+		#$MeshInstance.rotate_x(deg2rad(ROTSPEED))
+		velocity.x -= .1
 	if velocity.z > 0:
-		velocity.z -= .05
+		#$MeshInstance.rotate_x(deg2rad(ROTSPEED))
+		velocity.z -= .1
+
 	if velocity.x < 0:
-		velocity.x += .05
+		#$MeshInstance.rotate_x(deg2rad(ROTSPEED))
+		velocity.x += .1
 	if velocity.z < 0:
-		velocity.z += .05
+		#$MeshInstance.rotate_x(deg2rad(ROTSPEED))
+		velocity.z += .1
 		
-	if velocity.y > 0:
-		velocity.y -= 5 # end friction calculations
+#	if velocity.y > 0:
+#		velocity.y -= 5 # end friction calculations
 		
